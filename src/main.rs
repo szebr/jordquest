5c9d6335e538630723247924738fb108605de4bf
ian <ianbean911@gmail.com>
2023-09-20 10:11:26 -0400

    Add explicit WindowPlugin struct

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,21 @@ mod input;
 
 use bevy::prelude::*;
 
+const TITLE: &str = "LMFAOOOOO";
+const WIN_W: f32 = 1280.;
+const WIN_H: f32 = 720.;
+
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, jordquest::JordQuestPlugin))
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: TITLE.into(),
+                resolution: (WIN_W, WIN_H).into(),
+                present_mode: bevy::window::PresentMode::Fifo,
+                ..default()
+            }),
+            ..default()
+        }), jordquest::JordQuestPlugin))
         .run();
 }
 
