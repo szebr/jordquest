1df7df4bf8f8c0d0ca999d27f4a408656c9cfca5
Ruoheng Xu <xuruohengrd@gmail.com>
2023-09-28 18:37:43 -0400

    collision detection

--- src/game/enemy.rs
+++ src/game/enemy.rs
@@ -1,14 +1,17 @@
 use bevy::prelude::*;
 use bevy::prelude::shape::CapsuleUvProfile::Fixed;
+use bevy::sprite::collide_aabb::collide;
 use crate::player::Player;
 use crate::net;
+
+use super::map::TILE_SIZE;
 
 pub const MAX_ENEMIES: usize = 32;
 
 #[derive(Component, Default, Copy, Clone)]
 pub struct Enemy {
     id: usize,
-    pos: Vec2
+    pub pos: Vec2
 }
 
 pub struct EnemyPlugin;
@@ -26,7 +29,7 @@ pub fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         // FIXEDUPDATE
         Enemy {
             id: 0,
-            pos: Vec2::default()
+            pos: Vec2::new(300.0, 300.0)
         },
         // UPDATE
         SpriteBundle {
@@ -45,12 +48,24 @@ pub fn fixed(
     let speed = 100. / net::TICKRATE as f32;
     for mut en in &mut enemies {
         let closest_player = &players.iter().next();
-        if !closest_player.is_none() {
+        if let Some(player) = closest_player{
+            let player_size = Vec2::new(130.0, 130.0);//size of the player for collision detection
+            let enemy_size = Vec2::new(64.0, 64.0);//size of the enemy for collision detection
+
+            //collision detection
+            if collide(
+                Vec3::new(en.pos.x, en.pos.y, 0.0),
+                enemy_size,
+                Vec3::new(player.pos.x, player.pos.y, 0.0),
+                player_size
+            ).is_some() {
+                continue;//if collision happened, stop moving
+            }
             //TODO when there are multiple players, find the closest one
             /*for pl in &players {
             }*/
-            let closest_player = closest_player.unwrap();
-            let movement = closest_player.pos - en.pos;
+            //let closest_player = closest_player.unwrap();
+            let movement = player.pos - en.pos;
             if movement.length() < 0.1 { continue }
             let movement = movement.normalize();
             en.pos.x += movement.x * speed;
