b9cea98d2c9357c6c24b6b219061e5d7a639a76c
ian <ijw6@pitt.edu>
2023-10-16 21:03:49 -0400

    Remove old PositionBuffer

--- src/net/lerp.rs
+++ src/net/lerp.rs
@@ -1,29 +1,12 @@
 use bevy::prelude::*;
+use crate::game::buffers::PosBuffer;
 use crate::game::player::LocalPlayer;
 use crate::net;
-
-// TODO: Should this be moved?
-#[derive(Component)]
-pub struct PositionBuffer(pub [Vec2; net::BUFFER_SIZE]);
-
-impl PositionBuffer {
-    pub fn get(&self, tick: u16) -> &Vec2 {
-        let i = tick as usize % net::BUFFER_SIZE;
-        &self.0[i]
-    }
-
-    //TODO pub fn getMut(&mut self, tick:u16) -> &mut PlayerTick
-
-    pub fn set(&mut self, tick: u16, input: Vec2) {
-        let i = tick as usize % net::BUFFER_SIZE;
-        self.0[i] = input;
-    }
-}
 
 pub fn lerp_pos(
     tick_time: Res<FixedTime>,
     tick: Res<net::TickNum>,
-    mut query: Query<(&mut Transform, &PositionBuffer), Without<LocalPlayer>>,
+    mut query: Query<(&mut Transform, &PosBuffer), Without<LocalPlayer>>,
 ) {
     for (mut tf, bp) in &mut query {
         let next_state = bp.get(tick.0.wrapping_sub(net::DELAY));
